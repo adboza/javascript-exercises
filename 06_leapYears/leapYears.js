829c71f19d007652517be85180f11a424a6e9dd7
Adriana Boza <adboza@gmail.com>
2022-07-01 14:20:13 -0300

    Added exercises 06 e 07 JS Fund. TheOdinProject

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    if (year%400 === 0){
+        console.log(year);
+        return true;
+    }else if (year%100 === 0 ){
+        console.log(year);
+        return false;
+
+    }else if (year%4 ===0){
+        console.log(year);
+        return true;
+    }else{
+        console.log(year);
+        return false;
+    }
 
 };
+
+leapYears(1900);
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const ftoc = function() {
+const ftoc = function(faren) {
+  let celc = (faren - 32)*(5/9);
+  let celci = parseFloat(celc.toFixed( 1 ));
+  return celci;
 
 };
 
-const ctof = function() {
+const ctof = function(celc) {
+  let faren = (celc*9/5)+32;
+
+  let farenh = parseFloat(faren.toFixed( 1 ));
+  return farenh;
 
 };
 
