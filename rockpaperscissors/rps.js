7e3a0d028ad6f59f3e9ab0ea098b097650cb60bf
Adriana Boza <adboza@gmail.com>
2022-07-06 17:08:18 -0300

    Almost finished RPS challenge

--- rockpaperscissors/rps.js
+++ rockpaperscissors/rps.js
@@ -1,4 +1,26 @@
+var paper = document.getElementById('paper').addEventListener('click', playPaper);
 
+function playPaper(){
+    let playerSelection = 2;
+    computerchoice = computerPlay();
+    playRound(playerSelection, computerchoice);
+}
+
+var rock = document.getElementById('rock').addEventListener('click', playRock);
+
+function playRock(){
+    let playerSelection = 5;
+    computerchoice = computerPlay();
+    playRound(playerSelection, computerchoice);
+}
+
+var scissors = document.getElementById('scissors').addEventListener('click', playScissors);
+
+function playScissors(){
+    let playerSelection = 3;
+    computerchoice = computerPlay();
+    playRound(playerSelection, computerchoice);
+}
 
   function computerPlay(){
     let computerchoice = 77;
@@ -35,12 +57,28 @@
 
   }
   
-  let playerSelection = 3;
+ 
 
   const computerSelection = computerPlay();
 
   function playRound(playerSelection, computerSelection) {
     // your code here!
+   
+    /*let playerSelection;
+    if (button.value === "rock"){
+        playerSelection = 5;
+
+    }else if (button.value === "paper"){
+        playerSelection = 2; //paper
+    }else if(button.value === '#scissors'){
+        playerSelection = 3; //scissors
+
+    }else{
+        return;
+    }*/
+
+
+
     let choice = playerSelection - computerSelection;
     
 
@@ -48,25 +86,43 @@
     
             if (choice === 0){
                 console.log("It\'s a tie!")
+                document.getElementById('msg').textContent = 'It\'s a tie!';
+                document.getElementById('yourScore').textContent = '1';
+                    document.getElementById('cpScore').textContent = '1';
             }
             
         
             else if (choice === 1 || choice === -1){
                 console.log("Scissors beats paper!");
+                
                 if (choice > 0){
                     console.log("You Win!");
+                    document.getElementById('msg').textContent = 'Scissors beats paper! You Win!';
+                    document.getElementById('yourScore').textContent = '1';
+                    document.getElementById('cpScore').textContent = '0';
                 }else {
                     console.log("You Lose!");
+                    document.getElementById('msg').textContent = 'Scissors beats paper! You Lose!';
+                    document.getElementById('yourScore').textContent = '0';
+                    document.getElementById('cpScore').textContent = '1';
                 }
             }
             
         
             else if (choice === 2 || choice === -2){
                 console.log("Rock beats Scissors!");
+                
+                
                 if (choice > 0){
                     console.log("You Win!");
+                    document.getElementById('msg').textContent = 'Rock beats Scissors! You Win!';
+                    document.getElementById('yourScore').textContent = '1';
+                    document.getElementById('cpScore').textContent = '0';
                 }else {
                     console.log("You Lose!");
+                    document.getElementById('msg').textContent = 'Rock beats Scissors! You Lose!';
+                    document.getElementById('yourScore').textContent = '0';
+                    document.getElementById('cpScore').textContent = '1';
                 }
             }
             
@@ -76,8 +132,14 @@
                 console.log("Paper beats Rock!");
                 if (choice < 0){
                     console.log("You Win!");
+                    document.getElementById('msg').textContent = 'Paper beats Rock! You Win!';
+                    document.getElementById('yourScore').textContent = '1';
+                    document.getElementById('cpScore').textContent = '0';
                 }else {
                     console.log("You Lose!");
+                    document.getElementById('msg').textContent = 'Paper beats Rock! You Lose!';
+                    document.getElementById('yourScore').textContent = '0';
+                    document.getElementById('cpScore').textContent = '1';
                 }
             }
             
@@ -86,16 +148,3 @@
         }
     }
   
-
-  console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = 2;
-  console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = 5;
-  console.log(playRound(playerSelection, computerSelection));
-  playerSelection = 2;
-  console.log(playRound(playerSelection, computerSelection));
-
-  playerSelection = 5;
-  console.log(playRound(playerSelection, computerSelection));
